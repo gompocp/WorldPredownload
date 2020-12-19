696cfa46a7b8ef21e8b2cad59a1ee5d2bb3abe7e
gompocp <73906388+gompocp@users.noreply.github.com>
2020-12-19 18:13:40 +0000

    Update NotificationMenuListener.cs

--- Components/NotificationMenuListener.cs
+++ Components/NotificationMenuListener.cs
@@ -1,50 +1,29 @@
-﻿using System;
-using System.Runtime.InteropServices;
-using System.Reflection;
-using UnhollowerRuntimeLib;
+using System;
+using UnhollowerBaseLib.Attributes;
 using UnityEngine;
 
 namespace WorldPredownload.Components
 {
     class NotificationMenuListener : MonoBehaviour
     {
-        public Delegate ReferencedDelegate;
-        public IntPtr MethodInfo;
+        [method:HideFromIl2Cpp]
+        public event Action? OnEnabled;
+        
+        [method:HideFromIl2Cpp]
+        public event Action? OnDisabled;
+        
         public NotificationMenuListener(IntPtr obj0) : base(obj0)
         {
-            ClassInjector.DerivedConstructorBody(this);
         }
 
-        public NotificationMenuListener(Delegate referencedDelegate, IntPtr methodInfo) : base(ClassInjector.DerivedConstructorPointer<NotificationMenuListener>())
-        {
-            ClassInjector.DerivedConstructorBody(this);
-
-            ReferencedDelegate = referencedDelegate;
-            MethodInfo = methodInfo;
-        }
-        ~NotificationMenuListener()
-        {
-            Marshal.FreeHGlobal(MethodInfo);
-            MethodInfo = IntPtr.Zero;
-            ReferencedDelegate = null;
-        }
-        private void OnAwake()
-        {
-            if (OnAwakeMethod == null) return;
-            OnAwakeMethod.Invoke(null, new object[0]);
-        }
         private void OnEnable()
         {
-            if (OnEnableMethod == null) return;
-            OnEnableMethod.Invoke(null, new object[0]);
+            OnEnabled?.Invoke();
         }
+
         private void OnDisable()
         {
-            if (OnDisableMethod == null) return;
-            OnDisableMethod.Invoke(null, new object[0]);
+            OnDisabled?.Invoke();
         }
-        public static MethodInfo OnEnableMethod { get; set; }
-        public static MethodInfo OnDisableMethod { get; set; }
-        public static MethodInfo OnAwakeMethod { get; set; }
     }
 }
